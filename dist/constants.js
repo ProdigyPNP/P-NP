4bcd181d30849398eb18dc79d91c2c1482724860
gemsvidø <gemsvido@gmail.com>
2022-04-07 14:08:18 -0700

    Change Link

--- dist/constants.js
+++ dist/constants.js
@@ -2,4 +2,4 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.DOWNLOAD_LINK = exports.VERSION = void 0;
 exports.VERSION = "2.1.8";
-exports.DOWNLOAD_LINK = "https://github.com/Prodigy-Hacking/PHEx/raw/master/build/extension.zip";
+exports.DOWNLOAD_LINK = "https://raw.githubusercontent.com/ProdigyPNP/ProdigyMathGameHacking/master/PHEx/build/extension.zip";
