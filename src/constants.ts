5fc7bf414a04157dbe7280534583574f98992beb
gemsvidø <gemsvido@gmail.com>
2022-04-07 13:50:37 -0700

    Update downlaod link

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 export const VERSION = "2.1.8";
-export const DOWNLOAD_LINK = "https://github.com/Prodigy-Hacking/PHEx/raw/master/build/extension.zip";
+export const DOWNLOAD_LINK = "https://raw.githubusercontent.com/ProdigyPNP/ProdigyMathGameHacking/master/PHEx/build/extension.zip";
