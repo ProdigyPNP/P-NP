7a771e278ea5d652dab9a6a105ed3602897c3851
gemsvido <gemsvido@gmail.com>
2022-04-20 10:27:50 -0700

    P-NP 2.1.9

--- dist/constants.js
+++ dist/constants.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.DOWNLOAD_LINK = exports.VERSION = void 0;
-exports.VERSION = "2.1.8";
+exports.VERSION = "2.1.9";
 exports.DOWNLOAD_LINK = "https://raw.githubusercontent.com/ProdigyPNP/ProdigyMathGameHacking/master/PHEx/build/extension.zip";
