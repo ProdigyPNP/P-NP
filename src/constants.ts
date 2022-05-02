919698a3fc03fc64c4160266667382ffba603253
gemsvido <gemsvido@gmail.com>
2022-05-02 08:48:58 -0700

    BRINGING BACK SRC - #11

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const VERSION = "2.1.9";
+export const DOWNLOAD_LINK = "https://raw.githubusercontent.com/ProdigyPNP/ProdigyMathGameHacking/master/PHEx/build/extension.zip";
+export const GUI_LINK = "https://raw.githubusercontent.com/ProdigyPNP/ProdigyMathGameHacking/master/willsCheatMenu/dist/bundle.js";
