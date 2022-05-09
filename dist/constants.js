e57ec76af016bbe585a5344ca90e21f9bf6b6486
gemsvido <gemsvido@gmail.com>
2022-05-09 09:19:57 -0700

    Changing link...

--- dist/constants.js
+++ dist/constants.js
@@ -3,5 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.LICENSE_LINK = exports.GUI_LINK = exports.DOWNLOAD_LINK = exports.VERSION = void 0;
 exports.VERSION = "2.1.9";
 exports.DOWNLOAD_LINK = "https://raw.githubusercontent.com/ProdigyPNP/ProdigyMathGameHacking/master/PHEx/build/extension.zip";
-exports.GUI_LINK = "https://raw.githubusercontent.com/ProdigyPNP/ProdigyMathGameHacking/master/willsCheatMenu/dist/bundle.js";
+exports.GUI_LINK = "https://raw.githubusercontent.com/ProdigyPNP/ProdigyMathGameHacking/master/cheatGUI/dist/bundle.js";
 exports.LICENSE_LINK = "https://github.com/ProdigyPNP/ProdigyMathGameHacking/blob/master/LICENSE.txt";
--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const VERSION = "2.1.9";
 export const DOWNLOAD_LINK = "https://raw.githubusercontent.com/ProdigyPNP/ProdigyMathGameHacking/master/PHEx/build/extension.zip";
-export const GUI_LINK = "https://raw.githubusercontent.com/ProdigyPNP/ProdigyMathGameHacking/master/willsCheatMenu/dist/bundle.js";
+export const GUI_LINK = "https://raw.githubusercontent.com/ProdigyPNP/ProdigyMathGameHacking/master/cheatGUI/dist/bundle.js";
 export const LICENSE_LINK = "https://github.com/ProdigyPNP/ProdigyMathGameHacking/blob/master/LICENSE.txt";
