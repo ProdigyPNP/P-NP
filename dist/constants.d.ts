2e364bee56a034094971fda8e074c68f9b71c79a
gemsvido <gemsvido@gmail.com>
2022-07-17 19:30:54 -0700

    Getting ready for 2.2.0 (PHEx Infinity) [Infinite Zero]

--- dist/constants.d.ts
+++ dist/constants.d.ts
@@ -1,4 +1,4 @@
-export declare const VERSION = "2.1.9";
+export declare const VERSION = "2.2.0";
 export declare const DOWNLOAD_LINK = "https://raw.githubusercontent.com/ProdigyPNP/ProdigyMathGameHacking/master/PHEx/build/extension.zip";
 export declare const GUI_LINK = "https://raw.githubusercontent.com/ProdigyPNP/ProdigyMathGameHacking/master/cheatGUI/dist/bundle.js";
 export declare const LICENSE_LINK = "https://github.com/ProdigyPNP/ProdigyMathGameHacking/blob/master/LICENSE.txt";
