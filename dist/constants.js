df8bcfceea21615f5cd73472857be70c7376b80c
gemsvido <gemsvido@gmail.com>
2022-09-27 23:38:24 -0700

    see how this goes

--- dist/constants.js
+++ dist/constants.js
@@ -1,11 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.UNMINIFY_SOUCE = exports.SERVER_PORT = exports.LICENSE_LINK = exports.DOWNLOAD_LINK = exports.GUI_HISTORY = exports.GUI_LINK = exports.VERSION = void 0;
+exports.PRODUCTION = exports.HTTPS_CHAIN_PATH = exports.HTTPS_KEY_PATH = exports.HTTPS = exports.UNMINIFY_SOUCE = exports.HTTPS_PORT = exports.HTTP_PORT = exports.LICENSE_LINK = exports.DOWNLOAD_LINK = exports.GUI_HISTORY = exports.GUI_LINK = exports.VERSION = void 0;
 exports.VERSION = "3.0.1";
 exports.GUI_LINK = "" || "https://raw.githubusercontent.com/ProdigyPNP/ProdigyMathGameHacking/master/cheatGUI/dist/bundle.js";
-exports.GUI_HISTORY = false || false;
+exports.GUI_HISTORY = false;
 exports.DOWNLOAD_LINK = "" || "https://raw.githubusercontent.com/ProdigyPNP/ProdigyMathGameHacking/master/PHEx/build/extension.zip";
 exports.LICENSE_LINK = "" || "https://github.com/ProdigyPNP/ProdigyMathGameHacking/blob/master/LICENSE.txt";
-exports.SERVER_PORT = 0 || 1337;
-exports.UNMINIFY_SOUCE = false || false;
+exports.HTTP_PORT = 0 || 80;
+exports.HTTPS_PORT = 0 || 443;
+exports.UNMINIFY_SOUCE = false;
+exports.HTTPS = true;
+exports.HTTPS_KEY_PATH = "" || "/etc/letsencrypt/live/hacks.prodigypnp.com/privkey.pem";
+exports.HTTPS_CHAIN_PATH = "" || "/etc/letsencrypt/live/hacks.prodigypnp.com/fullchain.pem";
+exports.PRODUCTION = "" || "hacks.prodigypnp.com";
 //# sourceMappingURL=constants.js.map
